53f6f492e773237c759fd1e10757a54bcc8fbaf9
aurelien <aurellg@outlook.fr>
2018-04-04 07:55:41 +0200

    création des fichiers classes et du fichier main (game)

---
+++ badguys.py
@@ -0,0 +1,5 @@
+class Badguys :
+    # does not moove
+    # it's the end of my games
+    # He sleep when Mcgyver has all the item and he comes on him
+
---
+++ mcgyver.py
@@ -0,0 +1,5 @@
+class Mcgyver:
+    # number of boxes in one step = 1
+    # starting point = random-1
+    # he takes item while passing on
+    # mcgyver moove with the touchpad
