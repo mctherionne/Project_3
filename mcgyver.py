83301265ede415b69840b88baa6ef77e28203d34
aurelien <aurellg@outlook.fr>
2018-04-09 09:24:03 +0200

    creation class mcgyver

--- mcgyver.py
+++ mcgyver.py
@@ -1,5 +1,21 @@
-class Mcgyver:
-    # number of boxes in one step = 1
-    # starting point = random-1
-    # he takes item while passing on
-    # mcgyver moove with the touchpad
+import pygame
+from pygame.locals import *
+from constant import *
+from labyrinth import *
+class mcgyver:
+    def __init__(self, moove, level):
+        self.move = pygame.image.load(character).convert_alpha()
+        self.case_x = 0
+        self.case_y = 0
+        self.x = 0
+        self.y = 0
+        self.level = level
+
+
+    def move(self, direction):
+        if direction =='right':
+            if self.case_x < (number_sprite_side - 1):
+                if self.level.structure [self.case_y][self.case_x + 1] != "w"
+                    self.case_x += 1
+                    self.x = self.case_x * size_sprite
+
