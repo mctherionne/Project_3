95c2901ec11461820cec22d826a74201db84e2d4
aurelien <aurellg@outlook.fr>
2018-04-09 08:58:40 +0200

    put in code constant of my game

---
+++ constant.py
@@ -0,0 +1,16 @@
+# Window settings
+number_sprite_side = 15
+size_sprite = 30
+side_window = size_sprite * number_sprite_side
+
+# Customizing window
+window_title = "Run Mcgyver"
+Image_icon = "D:\Project_3\image/macgyver.png"
+
+# Lists of the game's images
+image_home = "D:\Project_3\image/home.jpg"
+image_background = "D:\Project_3\image/bacground.png"
+image_wall = "D:\Project_3\image/wall.png"
+image_loot = "D:\Project_3\image/loot.png"
+image_mcgyver = "D:\Project_3\image/macgyver.png"
+image_guard = "D:\Project_3\image/guard.png"
