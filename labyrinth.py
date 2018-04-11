c5fb5a183e246eef83e126c6ae630f90dde6b757
aurelien <aurellg@outlook.fr>
2018-04-11 08:49:34 +0200

    to advance on my mcgyver file and on the construction of the labyrinth file. mcgyver file : create a mcgyver class with moving and image labyrinth file : create the labyrinth class with the function generate

--- labyrinth.py
+++ labyrinth.py
@@ -19,3 +19,20 @@ class Labyrinth :
             self.structure = structure_level
 
     def display(self, window):
+        wall = pygame.image.load(image_wall).convert()
+        end_of_level = pygame.image.load(image_guard).convert()
+
+        num_line = 0
+        for sprite in line:
+            x = num_case * size_sprite
+            y = num_line * size_sprite
+            if sprite == 'm':
+                window.blit(wall,(x, y))
+            elif sprite =='d':
+                window.blit (start, (x, y))
+            elif sprite =='a':
+                window.blit (end_of_level, (x, y))
+            num_case += 1
+        num_line += 1
+
+
